7a72226be0fb544758253926afc278b199e031ce
jadehound <codeberg@rohit.top>
2023-07-04 15:23:43 +1000

    indexeddb initial load + error pgs

--- src/router.rs
+++ src/router.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use leptos::*;
 use leptos_router::*;
 
@@ -5,29 +7,31 @@ use crate::errors::*;
 use crate::roster::*;
 use crate::settings::*;
 
-#[derive(Clone)]
-pub struct IsLoaded(bool);
-
-#[component]
-pub fn RouterScout(cx: Scope) -> impl IntoView {
-    let (get_loaded, set_loaded) = create_signal(cx, IsLoaded(false));
-    provide_context(cx, set_loaded);
-    view! {
-        cx,
-        {if get_loaded.get().0 {
-            view!{ cx, <MainRouter /> }.into_view(cx)
-        } else {
-            view!{ cx, <LoadingPg /> }.into_view(cx)
+async fn initial_load(cx: Scope) -> bool {
+    match indxdb::new("wf").await {
+        Ok(db) => {
+            provide_context(cx, Rc::new(db));
+            true
         }
+        Err(e) => {
+            FatalErr::report(cx, "indxdb loading", e);
+            false
         }
     }
 }
 
 #[component]
-fn LoadingPg(cx: Scope) -> impl IntoView {
-    view! {
-        cx,
-        <h1 class= "flex items-center justify-center h-cover"> "Loading" </h1>
+pub fn RouterScout(cx: Scope) -> impl IntoView {
+    let load_assets = create_resource(cx, || (), move |_| async move { initial_load(cx).await });
+    view! { cx,
+        {move || match load_assets.read(cx) {
+            None => view! { cx, }.into_view(cx),
+            Some(success) => if success {
+                view! { cx, <MainRouter /> }.into_view(cx)
+            } else {
+                view! { cx, <FatalPg /> }.into_view(cx)
+            }
+        }}
     }
 }
 
